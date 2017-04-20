5a7c8276548ee601ae2b0fc2dffac73905d61938
daggerly <dagger003@163.com>
2017-04-20 13:21:56 +0800

    Create python列表去重.py

---
+++ python列表去重.py
@@ -0,0 +1,17 @@
+#要求给一个列表去重，不能使用集合或者字典
+#感觉这题没啥意义，因为在什么情况下我不能够在代码里使用集合或者字典呢
+a = [1,2,1,1,3,4]
+finish = 0
+while not finish:
+    for i,v in enumerate(a):
+        for j, vv in enumerate(a[i+1:], i+1):
+            if v == vv:
+                a.pop(j)
+                break
+        else:
+            continue
+        break
+    else:
+        finish = 1
+
+print a
